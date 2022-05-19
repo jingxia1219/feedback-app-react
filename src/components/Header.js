ac630797eb7a5f7c45b282b414034e41d5be4b48
jingxia1219 <jingxia1219@gmail.com>
2022-05-18 17:51:52 -0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+function Header(){
+    return (
+        <Header>
+          <div className='container'>
+              <h2>
+                  Feedback App
+              </h2>
+          </div>  
+        </Header>
+    )
+}
+
+export default Header;
